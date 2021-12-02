66ee7d23452909645713eb9a888ccb664c10fc64
YuJaeHyun <slade12307@gmail.com>
2021-12-02 18:40:46 +0900

    CRA tour

--- ReactJS/react-for-beginners/src/index.js
+++ ReactJS/react-for-beginners/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//import "./style.css";
+// 우리가 아는 방식인 css파일을 만들고 임포트하는 식으로 만들어도 됨!
+// 또다른 방식으로는 그냥 javascript상으로 style을 넣는 방식!
 
 ReactDOM.render(
   <React.StrictMode>

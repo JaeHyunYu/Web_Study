708f4156656631e4bbc0e9239e32a1842f2efb90
YuJaeHyun <slade12307@gmail.com>
2022-02-06 19:13:26 +0900

    update basic logo

---
+++ starbucks_clone/src/Nav.js
@@ -0,0 +1,20 @@
+import { MdCoffeeMaker, MdLocationOn, MdPerson } from 'react-icons/md';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import styles from './Nav.module.css';
+function Nav() {
+    return (
+        <div className={styles.wrap}>
+            <a href="/" className={styles.logo}></a>
+
+            <div className={styles.icons}>
+                <MdPerson />
+                <MdLocationOn />
+                <GiHamburgerMenu />
+
+
+            </div>
+        </div >
+    );
+}
+
+export default Nav;

62fc49ebd69170431cf402bb992d13600083f11e
YuJaeHyun <slade12307@gmail.com>
2022-02-09 21:44:43 +0900

    modify margin-option at index.module.css(delete) and update each css files

--- starbucks_clone/src/Section2.js
+++ starbucks_clone/src/Section2.js
@@ -1,8 +1,16 @@
+import styles from './css/Section2.module.css';
 function Section2() {
     return (
-        <div>
+        <div className={styles.wrap}>
+            <img className={styles.logo} src="https://image.istarbucks.co.kr/common/img/main/rewards-logo.png"></img>
+            <div>
+
+            </div>
+
+            <div>
 
 
+            </div>
         </div >
     );
 }

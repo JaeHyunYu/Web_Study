597340c2a26f45cf9168fe02f920a7965688c011
YuJaeHyun <slade12307@gmail.com>
2022-02-22 21:58:14 +0900

    Add some media query & some works arrording to media query

--- starbucks_clone/src/Nav.js
+++ starbucks_clone/src/Nav.js
@@ -1,15 +1,30 @@
-import { MdLocationOn, MdPerson } from 'react-icons/md';
+import { useState } from 'react';
+
+import { MdFormatIndentDecrease, MdLocationOn, MdPerson } from 'react-icons/md';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import styles from './css/Nav.module.css';
 function Nav() {
+
+    const lists = ['Sing in', 'My Starbucks', 'Customer Service & MdFormatIndentDecrease', 'Find a Store'];
     return (
         <div className={styles.wrap}>
+
+            <nav>
+                <ul>
+                    {lists.map(lists => <li className={styles.list}>{lists}</li>)}
+                </ul>
+            </nav>
+
             <a href="/" className={styles.logo}></a>
+            <div className={styles.wrap_inner}>
+            </div>
             <div className={styles.icons}>
                 <MdPerson />
                 <MdLocationOn />
                 <GiHamburgerMenu />
+
             </div>
+
         </div >
     );
 }

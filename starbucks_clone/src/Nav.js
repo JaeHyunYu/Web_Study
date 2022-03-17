e0b37e7468a841cbdb76f24e488f0f0f45a7ec7f
YuJaeHyun <slade12307@gmail.com>
2022-03-17 13:00:27 +0900

    wrap_inner1 proto

--- starbucks_clone/src/Nav.js
+++ starbucks_clone/src/Nav.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { MdFormatIndentDecrease, MdLocationOn, MdPerson } from 'react-icons/md';
+import { MdFormatIndentDecrease, MdLocationOn, MdPerson, MdSearch } from 'react-icons/md';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import styles from './css/Nav.module.css';
 function Nav() {
@@ -8,16 +8,21 @@ function Nav() {
     const lists = ['Sing in', 'My Starbucks', 'Customer Service & MdFormatIndentDecrease', 'Find a Store'];
     return (
         <div className={styles.wrap}>
+            <div className={styles.menus}>
+                <div className={styles.wrap_inner1}>
+                    <nav >
+                        <ul className={styles.ul}>
+                            {lists.map(lists => <li className={styles.list}><a href={"#"}>{lists}</a></li>)}
+                        </ul>
+                        <p><a href="#"><MdSearch></MdSearch></a></p>
+                    </nav>
+                </div>
+                <div className={styles.wrap_inner2}>
+                </div>
 
-            <nav>
-                <ul>
-                    {lists.map(lists => <li className={styles.list}>{lists}</li>)}
-                </ul>
-            </nav>
-
-            <a href="/" className={styles.logo}></a>
-            <div className={styles.wrap_inner}>
             </div>
+            <a href="/" className={styles.logo}></a>
+
             <div className={styles.icons}>
                 <MdPerson />
                 <MdLocationOn />

eacf3cafd6f81b354459b29f869179464fcc7992
YuJaeHyun <slade12307@gmail.com>
2022-03-17 19:46:19 +0900

    create nav_inner2 & modify notice width section

--- starbucks_clone/src/Nav.js
+++ starbucks_clone/src/Nav.js
@@ -6,18 +6,24 @@ import styles from './css/Nav.module.css';
 function Nav() {
 
     const lists = ['Sing in', 'My Starbucks', 'Customer Service & MdFormatIndentDecrease', 'Find a Store'];
+    const lists2 = ['COFFEE', 'MENU', 'STORE', 'RESPONSIBILITY', 'STARBUCKS REWARDS', 'WHAT\'S NEW'];
     return (
         <div className={styles.wrap}>
             <div className={styles.menus}>
                 <div className={styles.wrap_inner1}>
-                    <nav >
+                    <nav>
                         <ul className={styles.ul}>
-                            {lists.map(lists => <li className={styles.list}><a href={"#"}>{lists}</a></li>)}
+                            {lists.map(lists => <li className={styles.list} key={lists.id}><a href={"#"}>{lists}</a></li>)}
                         </ul>
                         <p><a href="#"><MdSearch></MdSearch></a></p>
                     </nav>
                 </div>
                 <div className={styles.wrap_inner2}>
+                    <nav>
+                        <ul className={styles.ul}>
+                            {lists2.map(lists => <li className={styles.list2}><a href={"#"}>{lists}</a></li>)}
+                        </ul>
+                    </nav>
                 </div>
 
             </div>

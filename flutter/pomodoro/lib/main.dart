51211f095b77d4ac044387c112638e618bc1cd6a
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2025-03-04 23:00:50 +0900

    Create main.dart

---
+++ flutter/pomodoro/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(
+          0xFFE7626C,
+        ),
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: Color(0xFFF4EDDB),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}

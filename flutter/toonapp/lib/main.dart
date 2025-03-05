e7bce2308c864a712b9a747e9993dde160f5fa2a
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2025-03-05 23:49:04 +0900

    Create main.dart

---
+++ flutter/toonapp/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:real_toonapp/services/api_service.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  ApiService().getTodaysToons();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen());
+  }
+}

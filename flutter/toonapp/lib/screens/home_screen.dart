cc21268240d29e9bf9cb61e055bcd67fafd05d0d
Jaehyun Yu <45021096+JaeHyunYu@users.noreply.github.com>
2025-04-13 21:40:48 +0900

    Update home_screen.dart

--- flutter/toonapp/lib/screens/home_screen.dart
+++ flutter/toonapp/lib/screens/home_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:real_toonapp/models/webtoon_model.dart';
+import 'package:real_toonapp/services/api_service.dart';
+import 'package:real_toonapp/widgets/webtoon_widget.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons =
+      ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -23,6 +30,49 @@ class HomeScreen extends StatelessWidget {
         elevation:
             1, // app바 하단 음영색 surfaceTintColor, shadowColor 둘다 설정해줘야함
       ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                SizedBox(height: 50),
+                Expanded(
+                  child: makeList(snapshot),
+                ),
+              ],
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeList(
+    AsyncSnapshot<List<WebtoonModel>> snapshot,
+  ) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        print(index);
+        var webtoon = snapshot.data![index];
+        return Webtoon(
+          title: webtoon.title,
+          thumb: webtoon.thumb,
+          id: webtoon.id,
+        );
+      },
+      separatorBuilder:
+          (context, index) =>
+              const SizedBox(width: 40),
     );
   }
 }
